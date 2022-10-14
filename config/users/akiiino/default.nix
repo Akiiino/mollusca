86437219a3870f3a73c87c7cf4b2260ebf275cd1
akiiino <git@akiiino.me>
2022-10-14 17:10:07 +0200

    Move files

--- config/users/akiiino/default.nix
+++ config/users/akiiino/default.nix
@@ -13,27 +13,7 @@
       ../../modules/git.nix
       ../../modules/kitty.nix
       ../../modules/gnome.nix
+      ./home.nix
     ];
-
-    home.packages = with pkgs; [
-      gimp
-      tdesktop
-      spotify
-      keepassxc
-      discord
-      protonvpn-gui
-      obsidian
-      dolphin-emu
-      vlc
-      slack
-      libreoffice
-      shotwell
-    ];
-    programs.kakoune.enable = true;
-    programs.bash.enable = true;
-    programs.zsh.enable = true;
-
-    home.stateVersion = "22.05";
   };
-
 }
---
+++ config/users/akiiino/home.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, ... }: {
+    home.packages = with pkgs; [
+      gimp
+      tdesktop
+      spotify
+      keepassxc
+      discord
+      protonvpn-gui
+      obsidian
+      dolphin-emu
+      vlc
+      slack
+      libreoffice
+      shotwell
+    ];
+    programs.kakoune.enable = true;
+    programs.bash.enable = true;
+    programs.zsh.enable = true;
+
+    home.stateVersion = "22.05";
+}
