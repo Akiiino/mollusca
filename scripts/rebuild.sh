e991f3e96cc929e11ccd5bef8875f34656495864
akiiino <13392695+Akiiino@users.noreply.github.com>
2023-03-12 20:04:37 +0100

    Fix rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,7 @@ decrypt secrets/minor_secrets.enc.json
 git add -N secrets/minor_secrets.json
 
 if [ "$1" = "localhost" ]; then
-    nixos-rebuild switch --flake ".#$2"
+    sudo nixos-rebuild switch --flake ".#$2"
 else
     nixos-rebuild switch --flake ".#$2" --fast --target-host "$1" --build-host "$1" --use-remote-sudo
 fi
