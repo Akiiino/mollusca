8d9f9e9a3eb569eb231699cf2a02a2156523f8f8
Akiiino <13392695+Akiiino@users.noreply.github.com>
2024-07-04 15:20:37 +0200

    Add new git alias

--- machines/workbook/default.nix
+++ machines/workbook/default.nix
@@ -67,6 +67,7 @@ in {
         userEmail = config.mollusca.secrets.workEmail;
         lfs.enable = true;
         aliases = {
+          "git" = "! cd -- \${GIT_PREFIX:-.} && git";
           "fpull" = "! f() { git fetch origin \"$1\":\"$1\"; }; f";
           "remote-main" = "! git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'";
           "move-branch" = "! f() { ONTO=$1 BRANCH=\${2:-$(git branch --show-current)} FROM=\${3:-$(git remote-main)}; git rebase --onto $ONTO $(git merge-base $FROM $BRANCH) $BRANCH; }; f";
