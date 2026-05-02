e050d548c27a120533e95b02588f893002194913
akiiino <13392695+Akiiino@users.noreply.github.com>
2026-05-02 22:55:48 +0200

    aspersum: simplify adb

--- machines/aspersum/default.nix
+++ machines/aspersum/default.nix
@@ -21,7 +21,6 @@
   # };
 
   users.users.akiiino = {
-    extraGroups = [ "adbusers" ];
     hashedPassword = "$6$nwRe8GAT99X9XVMD$EI8wRSBQF.zw6Evh7UVFKxfu/K9v2.i4hb1unxSnf26e50glpz6SkuVR9MQYr7/m.1IqgrstKvnPAVPa1i/JB0";
   };
 
@@ -149,6 +148,7 @@
         "rus"
       ];
     })
+    pkgs.android-tools-xdg
   ];
 
   programs = {
--- overlays/XDG_fixes.nix
+++ overlays/XDG_fixes.nix
@@ -1,14 +1,14 @@
 # Replaces apps with wrapped versions that respect XDG.
 
 final: prev: {
-  android-tools = prev.android-tools.overrideAttrs (oldAttrs: {
+  android-tools-xdg = prev.android-tools.overrideAttrs (oldAttrs: {
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ final.makeWrapper ];
     postFixup = (oldAttrs.postFixup or "") + ''
       wrapProgram $out/bin/adb \
         --run 'export HOME="''${XDG_DATA_HOME:-$HOME/.local/share}/android"'
     '';
   });
-  wget = prev.wget.overrideAttrs (oldAttrs: {
+  wget-xdg = prev.wget.overrideAttrs (oldAttrs: {
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ final.makeWrapper ];
     postFixup = (oldAttrs.postFixup or "") + ''
       wrapProgram $out/bin/wget \
