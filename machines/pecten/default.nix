565e0c9391d6d7dfe50b00bb4482f691c6633c13
akiiino <13392695+Akiiino@users.noreply.github.com>
2026-07-16 19:01:05 +0200

    chore: move pecten's headless tweaks into the headless profile

--- machines/pecten/default.nix
+++ machines/pecten/default.nix
@@ -1,7 +1,5 @@
 {
-  lib,
   modulesPath,
-  inputs,
   config,
   self,
   minor-secrets,
@@ -27,37 +25,6 @@
     chatId = minor-secrets.telegramId;
   };
   age.secrets.eunomia-telegram.file = "${self}/secrets/eunomia-telegram.age";
-  # TODO: move to `mollusca.remote` or `mollusca.headless`?
-  system.tools = {
-    nixos-rebuild.enable = false;
-    nixos-install.enable = false;
-    nixos-generate-config.enable = false;
-    nixos-enter.enable = false;
-    nixos-build-vms.enable = false;
-    nixos-option.enable = false;
-  };
-
-  xdg.mime.enable = false; # not needed on a headless server
-
-  # To resolve nixpkgs (e.g. nix run nixpkgs#hello) lazily,
-  # instead of building it into the closure (saves ~300 MB):
-
-  nixpkgs.flake.source = lib.mkForce null;
-  nix = {
-    registry.nixpkgs = {
-      from = {
-        type = "indirect";
-        id = "nixpkgs";
-      };
-      to = {
-        type = "github";
-        owner = "NixOS";
-        repo = "nixpkgs";
-        inherit (inputs.nixpkgs) rev;
-      };
-    };
-    settings.extra-nix-path = "nixpkgs=flake:nixpkgs";
-  };
 
   networking = {
     hostName = "pecten";
--- profiles/headless.nix
+++ profiles/headless.nix
@@ -1,6 +1,42 @@
 {
+  lib,
+  inputs,
+  ...
+}:
+{
   mollusca = {
     isRemote = true;
     useTailscale = true;
+  };
+
+  system.tools = {
+    nixos-rebuild.enable = false;
+    nixos-install.enable = false;
+    nixos-generate-config.enable = false;
+    nixos-enter.enable = false;
+    nixos-build-vms.enable = false;
+    nixos-option.enable = false;
+  };
+
+  xdg.mime.enable = false;
+
+  # To resolve nixpkgs (e.g. nix run nixpkgs#hello) lazily,
+  # instead of building it into the closure (saves ~300 MB):
+
+  nixpkgs.flake.source = lib.mkForce null;
+  nix = {
+    registry.nixpkgs = {
+      from = {
+        type = "indirect";
+        id = "nixpkgs";
+      };
+      to = {
+        type = "github";
+        owner = "NixOS";
+        repo = "nixpkgs";
+        inherit (inputs.nixpkgs) rev;
+      };
+    };
+    settings.extra-nix-path = "nixpkgs=flake:nixpkgs";
   };
 }
