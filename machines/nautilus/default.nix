4b1b65e0dab35102661b639346d83d57e33d194b
akiiino <13392695+Akiiino@users.noreply.github.com>
2026-07-21 09:09:29 +0200

    nautilus: switch to stable NVIDIA drivers

--- machines/nautilus/default.nix
+++ machines/nautilus/default.nix
@@ -79,16 +79,7 @@
 
       nvidiaSettings = true;
       powerManagement.enable = true;
-      # TODO: remove after https://github.com/ValveSoftware/gamescope/issues/1964 is fixed
-      # or >580 is available
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "575.64.05";
-        sha256_64bit = "sha256-hfK1D5EiYcGRegss9+H5dDr/0Aj9wPIJ9NVWP3dNUC0=";
-        sha256_aarch64 = "sha256-GRE9VEEosbY7TL4HPFoyo0Ac5jgBHsZg9sBKJ4BLhsA=";
-        openSha256 = "sha256-mcbMVEyRxNyRrohgwWNylu45vIqF+flKHnmt47R//KU=";
-        settingsSha256 = "sha256-o2zUnYFUQjHOcCrB0w/4L6xI1hVUXLAWgG2Y26BowBE=";
-        persistencedSha256 = "sha256-2g5z7Pu8u2EiAh5givP5Q1Y4zk4Cbb06W37rf768NFU=";
-      };
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 
